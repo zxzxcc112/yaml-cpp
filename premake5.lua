3860be00728cd1655a8900e0f9fc99f8ddfb237a
PikaMax <pikazxzxcc112@gmail.com>
2024-08-30 12:07:34 +0900

    Add premake and vs project

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputDir .. "%{prj.name}")
+	objdir ("bin-int/" .. outputDir .. "%{prj.name}")
+
+	files
+	{
+		"include/**",
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"YAML_CPP_STATIC_DEFINE",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+		disablewarnings
+		{
+			"4819"
+		}
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release or Distribution"
+		runtime "Release"
+		optimize "on"
